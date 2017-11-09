9ca65199d95c2ad04cea655e934eb783f4c2e740
Jesus Cortez <jic619@yahoo.com>
2017-11-09 03:59:42 +0000

    fixed angular path issue and adding routes folder

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router({ caseSensitive: true });
+
+//Register
+
+router.post('/register', function(request,response){
+    console.log(request.body);
+});
+
+
+
+
+
+module.exports = router;
